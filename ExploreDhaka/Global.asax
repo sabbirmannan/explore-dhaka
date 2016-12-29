ff11a7eb712ffcfbb744bfb5c7d258c278c9923e
Abdullah Ibn Mannan <sabbir.mannan@gmail.com>
2016-12-29 12:01:51 +0600

    initial setup

---
+++ ExploreDhaka/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExploreDhaka.MvcApplication" Language="C#" %>
---
+++ ExploreDhaka/Models/Tour.cs
@@ -0,0 +1,13 @@
+﻿namespace ExploreDhaka.Models
+{
+    public class Tour
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Length { get; set; }
+        public decimal Price { get; set; }
+        public string Rating { get; set; }
+        public bool IncludesMeals { get; set; }
+    }
+}
